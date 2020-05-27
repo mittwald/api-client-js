a3d2caeb14aaf681378f92dd2a9799f66fbebcfb
Marco Falkenberg <m.falkenberg@mittwald.de>
2020-05-27 16:45:03 +0200

    Add option to manipulate request at runtime

--- packages/api-client/src/http/KyClient.ts
+++ packages/api-client/src/http/KyClient.ts
@@ -11,33 +11,46 @@ const d = debug.extend("KyHTTPClient");
 
 export type Ky = typeof ky;
 
-export interface Options extends KyOptions {
+const defaultOptions: Required<Options> = {
+    defaultHeaders: {},
+    requestOptionsHook: (opts) => opts,
+    ky: {},
+};
+
+export type RequestOptionsHook = (options: Options) => Options;
+
+export interface Options {
+    ky?: KyOptions;
     defaultHeaders?: Headers;
+    requestOptionsHook?: RequestOptionsHook;
 }
 
 export class KyClient implements Client {
     public readonly ky: Ky;
-    private defaultHeaders: Headers = {};
+    private readonly options: Required<Options>;
 
-    public constructor(defaultOptions: Options = {}) {
-        const { defaultHeaders, ...kyOnlyOptions } = defaultOptions;
-        this.defaultHeaders = defaultHeaders || {};
-        this.ky = ky.create(kyOnlyOptions);
+    public constructor(options: Options = {}) {
+        this.options = { ...defaultOptions, ...options };
+        this.ky = ky.create(this.options.ky);
     }
 
     public setDefaultHeaders(headers: Headers): void {
-        this.defaultHeaders = headers;
+        this.options.defaultHeaders = headers;
     }
 
     public requestFunctionFactory: RequestFunctionFactory = (descriptor) => async (request) => {
         const { path, method } = descriptor;
         const { header, requestBody, path: pathParams, query } = request;
 
+        // make a shallow copy
+        const options = this.options.requestOptionsHook({ ...this.options, ky: { ...this.options.ky } });
+
         try {
             const requestOptions: KyOptions = {
+                ...options.ky,
                 method,
                 headers: {
-                    ...this.defaultHeaders,
+                    ...options.defaultHeaders,
                     ...mapHeaders(header),
                 },
                 searchParams: query,
