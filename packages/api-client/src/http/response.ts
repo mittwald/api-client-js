c95a1b758fb6cc3094beda179b08534df89c61bf
Marco Falkenberg <m.falkenberg@mittwald.de>
2020-08-12 16:08:24 +0200

    Do not parse body only if bodyUsed is explicitly set to false
    Some clients do not support 'bodyUsed'

--- packages/api-client/src/http/response.ts
+++ packages/api-client/src/http/response.ts
@@ -4,7 +4,7 @@ import { mapHeaders } from "./headers";
 export const mapResponse = async (response: Response): Promise<Client.Response> => {
     return {
         status: response.status,
-        content: response.bodyUsed ? await response.json() : undefined,
+        content: response.bodyUsed === false ? undefined : await response.json(),
         header: mapHeaders(response.headers),
     };
 };
