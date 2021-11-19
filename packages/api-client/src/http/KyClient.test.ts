b02831bf2671bd153243c0a89dec09aadb7b88c0
Ruven Fehling <r.fehling@mittwald.de>
2021-11-19 16:45:58 +0100

    Fix query array mapping for integration test & frontends

---
+++ packages/api-client/src/http/KyClient.test.ts
@@ -0,0 +1,85 @@
+import { KyClient } from "./KyClient";
+
+describe("KyClient", () => {
+    test("query array param with multiple ids", async () => {
+        const client = new KyClient();
+
+        const handler = client.requestFunctionFactory({
+            method: "get",
+            operationId: "foo",
+            path: "https://jsonplaceholder.typicode.com/todos",
+        });
+
+        const response = await handler({
+            query: {
+                id: ["2", "3"],
+            },
+        });
+        return expect(response.content).toMatchObject([
+            {
+                completed: false,
+                id: 2,
+                title: "quis ut nam facilis et officia qui",
+                userId: 1,
+            },
+            {
+                completed: false,
+                id: 3,
+                title: "fugiat veniam minus",
+                userId: 1,
+            },
+        ]);
+    });
+
+    test("query array param with single id", async () => {
+        const client = new KyClient();
+
+        const handler = client.requestFunctionFactory({
+            method: "get",
+            operationId: "foo",
+            path: "https://jsonplaceholder.typicode.com/todos",
+        });
+
+        const response = await handler({
+            query: {
+                id: ["2"],
+            },
+        });
+
+        return expect(response.content).toMatchObject(
+            [
+                {
+                    completed: false,
+                    id: 2,
+                    title: "quis ut nam facilis et officia qui",
+                    userId: 1,
+                },
+            ],
+        );
+    });
+
+    test("path param with single id", async () => {
+        const client = new KyClient();
+
+        const handler = client.requestFunctionFactory({
+            method: "get",
+            operationId: "foo",
+            path: "https://jsonplaceholder.typicode.com/todos/{todo}",
+        });
+
+        const response = await handler({
+            path: {
+                todo: 2,
+            },
+        });
+
+        return expect(response.content).toMatchObject(
+            {
+                completed: false,
+                id: 2,
+                title: "quis ut nam facilis et officia qui",
+                userId: 1,
+            }
+        );
+    });
+});
