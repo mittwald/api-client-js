69c6bee1beade7359e011f3b98f8e40734f50514
Marco Falkenberg <m.falkenberg@mittwald.de>
2022-05-10 11:37:27 +0200

    Remove unused imports

--- packages/react-api-hooks/src/createRefreshCache.ts
+++ packages/react-api-hooks/src/createRefreshCache.ts
@@ -1,7 +1,5 @@
 import { OperationDescriptor, RequestFunction, RequestType } from "@mittwald/api-client/dist/OperationDescriptor";
-import { Resource } from "@mittwald/flow-lib/dist/resources/Resource";
 import { apiResourceStore } from "./apiResourceStore";
-import { ApiResource } from "./ApiResource";
 
 export const createRefreshCache = <T extends OperationDescriptor>(operationDescriptor: T) => <TRequestParams extends RequestType<T>>(
     requestParams: TRequestParams,
