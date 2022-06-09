1edbba84c7a6760bb867ff4b4e471bad0c257416
Marco Falkenberg <m.falkenberg@mittwald.de>
2022-06-09 09:00:45 +0200

    Fix lint and test

--- packages/api-client/src/RequestError.ts
+++ packages/api-client/src/RequestError.ts
@@ -4,8 +4,8 @@ export class RequestError extends Error {
     public readonly cause?: unknown;
 
     public constructor(path: string, method: string, cause?: unknown) {
-        const causeMessage = cause instanceof Error ? cause.message : undefined;
-        super(`${method.toUpperCase()} ${path} failed${causeMessage}` ? `: ${causeMessage}` : "");
+        const causeMessage = cause instanceof Error ? `: ${cause.message}` : "undefined";
+        super(`${method.toUpperCase()} ${path} failed${causeMessage}`);
         this.name = "RequestError";
         this.path = path;
         this.method = method.toUpperCase();
