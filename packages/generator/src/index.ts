fa43412bb9352b9b0223993520a6270553b2e505
Lukas Fritze <l.fritze@mittwald.de>
2024-03-07 17:10:31 +0100

    Export TypeScript API from generator package

---
+++ packages/generator/src/index.ts
@@ -0,0 +1,7 @@
+export * from "./loading/UniversalContentLoader.js";
+export * from "./loading/UniversalFileLoader.js";
+export * from "./openapi/OpenApiSpec.js";
+export * from "./generation/model/CodeGenerationModel.js";
+export * from "./generation/prepareTypeScriptOutput.js";
+export * from "./loading/UniversalFileLoader.js";
+export * from "./lib/writeIfChangedAsync.js";
