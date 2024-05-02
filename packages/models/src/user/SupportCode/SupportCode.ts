ded646494abfc6f5b0a62990bab017db4503e649
lmeyer <l.meyer@mittwald.de>
2024-05-02 07:59:05 +0200

    feat(SupportCode): add SupportCode model

---
+++ packages/models/src/user/SupportCode/SupportCode.ts
@@ -0,0 +1,36 @@
+import { ReferenceModel } from "../../base/ReferenceModel.js";
+import { provideReact } from "../../lib/provideReact.js";
+import { config } from "../../config/config.js";
+import { DataModel } from "../../base/DataModel.js";
+import { classes } from "polytype";
+import { SupportCodeData } from "./types.js";
+
+export class SupportCode extends ReferenceModel {
+  public static ofId(id: string): SupportCode {
+    return new SupportCode(id);
+  }
+
+  public static get = provideReact(async (): Promise<SupportCodeDetailed> => {
+    const data = await config.behaviors.supportCode.get();
+
+    return new SupportCodeDetailed(data);
+  });
+}
+
+class SupportCodeCommon extends classes(
+  DataModel<SupportCodeData>,
+  SupportCode,
+) {
+  public constructor(data: SupportCodeData) {
+    super([data], [data.supportCode]);
+  }
+}
+
+export class SupportCodeDetailed extends classes(
+  SupportCodeCommon,
+  DataModel<SupportCodeData>,
+) {
+  public constructor(data: SupportCodeData) {
+    super([data], [data]);
+  }
+}
---
+++ packages/models/src/user/index.ts
@@ -0,0 +1,1 @@
+export * from "./SupportCode/index.js";
