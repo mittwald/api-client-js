48dd173cdc7e4cdfa5a7ad136db8352424b6d0d8
Marco Falkenberg <m.falkenberg@mittwald.de>
2024-08-22 11:53:46 +0200

    Revert "feat(Models): export Query base models"
    This reverts commit e79674f3d73817d4f0ca9a1c14b15e0997a47729.

--- packages/models/src/base/index.ts
+++ packages/models/src/base/index.ts
@@ -1,6 +1,4 @@
 export * from "./assertObjectFound.js";
 export * from "./DataModel.js";
 export * from "./ReferenceModel.js";
-export * from "./ListDataModel.js";
-export * from "./ListQueryModel.js";
 export * from "./types.js";
