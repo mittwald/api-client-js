9bd6a8bd07154763043d2ac235c4f6445cc4f7b3
Marco Falkenberg <m.falkenberg@mittwald.de>
2024-09-04 14:44:27 +0200

    feat(Client): re-export * from commons

--- packages/mittwald/src/index.ts
+++ packages/mittwald/src/index.ts
@@ -3,5 +3,6 @@ export {
   assertOneOfStatus,
   extractTotalCountHeader,
 } from "@mittwald/api-client-commons";
+export * as Commons from "@mittwald/api-client-commons";
 export { MittwaldAPIClient as MittwaldAPIV2Client } from "./v2/default.js";
 export type { MittwaldAPIV2 } from "./generated/v2/types.js";
