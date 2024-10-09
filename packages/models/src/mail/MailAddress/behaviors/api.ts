4aec9f3f9c85b119f73397ec1936c65abc793e57
ntdoJanneck <janneckf03@gmail.com>
2024-10-09 15:22:10 +0200

    feat(mailaddress-model): add update api behaviors

--- packages/models/src/mail/MailAddress/behaviors/api.ts
+++ packages/models/src/mail/MailAddress/behaviors/api.ts
@@ -7,16 +7,14 @@ import { MailAddressBehaviors } from "./types.js";
 export const apiMailAddressBehaviors = (
   client: MittwaldAPIV2Client,
 ): MailAddressBehaviors => ({
-  find: async (id) => {
-    const response = await client.mail.getMailAddress({
-      mailAddressId: id,
-    });
+  find: async (mailAddressId: string) => {
+    const response = await client.mail.getMailAddress({ mailAddressId });
     if (response.status === 200) {
       return response.data;
     }
     assertOneOfStatus(response, [404]);
   },
-  query: async (projectId) => {
+  query: async (projectId: string) => {
     const response = await client.mail.listMailAddresses({
       projectId,
     });
@@ -25,5 +23,72 @@ export const apiMailAddressBehaviors = (
       items: response.data,
       totalCount: response.data.length,
     };
+  },
+  delete: async (mailAddressId: string) => {
+    const response = await client.mail.deleteMailAddress({ mailAddressId });
+    assertStatus(response, 204);
+  },
+  updateAddress: async (mailAddressId: string, address: string) => {
+    const response = await client.mail.updateMailAddressAddress({
+      mailAddressId,
+      data: {
+        address,
+      },
+    });
+    assertStatus(response, 204);
+  },
+  updatePassword: async (mailAddressId: string, password: string) => {
+    const response = await client.mail.updateMailAddressPassword({
+      mailAddressId,
+      data: {
+        password,
+      },
+    });
+    assertStatus(response, 204);
+  },
+  updateSpamProtection: async (
+    mailAddressId: string,
+    active: boolean,
+    autoDeleteSpam: boolean,
+    folder: "inbox" | "spam",
+    relocationMinSpamScore: number,
+  ) => {
+    const response = await client.mail.updateMailAddressSpamProtection({
+      mailAddressId,
+      data: {
+        spamProtection: {
+          active,
+          autoDeleteSpam,
+          folder,
+          relocationMinSpamScore,
+        },
+      },
+    });
+    assertStatus(response, 204);
+  },
+  updateQuotaInBytes: async (mailAddressId: string, bytes: number) => {
+    const response = await client.mail.updateMailAddressQuota({
+      mailAddressId,
+      data: { quotaInBytes: bytes },
+    });
+    assertStatus(response, 204);
+  },
+  updateForwards: async (mailAddressId: string, forwards: string[]) => {
+    const response = await client.mail.updateMailAddressForwardAddresses({
+      mailAddressId,
+      data: {
+        forwardAddresses: forwards,
+      },
+    });
+    assertStatus(response, 204);
+  },
+  updateCatchAll: async (mailAddressId: string, enabled: boolean) => {
+    const response = await client.mail.updateMailAddressCatchAll({
+      mailAddressId,
+      data: {
+        active: enabled,
+      },
+    });
+    assertStatus(response, 204);
   },
 });
--- packages/models/src/mail/MailAddress/behaviors/types.ts
+++ packages/models/src/mail/MailAddress/behaviors/types.ts
@@ -2,8 +2,23 @@ import { MailAddressData, MailAddressListItemData } from "../types.js";
 import { QueryResponseData } from "../../../base/index.js";
 
 export interface MailAddressBehaviors {
-  find: (id: string) => Promise<MailAddressData | undefined>;
+  find: (mailAddressId: string) => Promise<MailAddressData | undefined>;
   query: (
     projectId: string,
   ) => Promise<QueryResponseData<MailAddressListItemData>>;
+
+  delete: (mailAddressId: string) => Promise<void>;
+
+  updateAddress: (mailAddressId: string, address: string) => Promise<void>;
+  updatePassword: (mailAddressId: string, password: string) => Promise<void>;
+  updateSpamProtection: (
+    mailAddressId: string,
+    active: boolean,
+    autoDeleteSpam: boolean,
+    folder: "inbox" | "spam",
+    relocationMinSpamScore: number,
+  ) => Promise<void>;
+  updateQuotaInBytes: (mailAddressId: string, bytes: number) => Promise<void>;
+  updateForwards: (mailAddressId: string, forwards: string[]) => Promise<void>;
+  updateCatchAll: (mailAddressId: string, enabled: boolean) => Promise<void>;
 }
