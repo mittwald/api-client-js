3b94024b731772fea71fd38f44933224497a2383
martin-helmich <martin-helmich@users.noreply.github.com>
2025-07-14 19:05:59 +0000

    feat: update generated client

--- packages/mittwald/src/generated/v2/types.ts
+++ packages/mittwald/src/generated/v2/types.ts
@@ -11925,6 +11925,7 @@ export declare module MittwaldAPIV2 {
           namespace $200 {
             namespace Content {
               export interface ApplicationJson {
+                reason?: string;
                 valid: boolean;
               }
             }
