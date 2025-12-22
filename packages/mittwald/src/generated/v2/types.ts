7216baa18c7f267ce08b8bf9c05111db36f42cf3
mfal <mfal@users.noreply.github.com>
2025-12-22 19:09:02 +0000

    chore: update generated development client

--- packages/mittwald/src/generated/v2/types.ts
+++ packages/mittwald/src/generated/v2/types.ts
@@ -6046,7 +6046,6 @@ export declare module MittwaldAPIV2 {
         isAllowedToPlaceOrders?: boolean;
         isBanned?: boolean;
         isInDefaultOfPayment?: boolean;
-        isMailAddressInvalid?: boolean;
         levelOfUndeliverableDunningNotice?: "first" | "second";
         memberCount: number;
         name: string;
