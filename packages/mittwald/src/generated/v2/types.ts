17b70dd12ec2c040f5393440d18c86887e1bfc97
martin-helmich <martin-helmich@users.noreply.github.com>
2026-02-27 19:15:25 +0000

    feat: update generated client

--- packages/mittwald/src/generated/v2/types.ts
+++ packages/mittwald/src/generated/v2/types.ts
@@ -9410,21 +9410,15 @@ export declare module MittwaldAPIV2 {
         | "storageAsc"
         | "storageDesc";
 
+      export interface LicenseKey {
+        key: string;
+      }
+
       export interface LicenseAppVersionMeta {
         description: string;
       }
 
       export type LicenseKind = "typo3-elts";
-
-      export interface LicenseMeta {
-        appVersion?: MittwaldAPIV2.Components.Schemas.LicenseAppVersionMeta;
-      }
-
-      export interface LicenseReference {
-        aggregate: "project";
-        domain: "project";
-        id: string;
-      }
 
       export interface LicenseLicense {
         description: string;
@@ -9439,18 +9433,24 @@ export declare module MittwaldAPIV2 {
         volume?: number;
       }
 
-      export interface LicenseExternalKey {
-        externalKey: string;
+      export interface LicenseReference {
+        aggregate: "project";
+        domain: "project";
+        id: string;
       }
 
-      export interface LicenseKey {
-        key: string;
+      export interface LicenseMeta {
+        appVersion?: MittwaldAPIV2.Components.Schemas.LicenseAppVersionMeta;
       }
 
       export interface LicenseKeyResponse {
         keyReference?:
           | MittwaldAPIV2.Components.Schemas.LicenseKey
           | MittwaldAPIV2.Components.Schemas.LicenseExternalKey;
+      }
+
+      export interface LicenseExternalKey {
+        externalKey: string;
       }
 
       export type OrderLicenseOrder = {
