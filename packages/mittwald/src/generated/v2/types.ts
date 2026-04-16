b6b7d320a62566b75d383177c70889966a2dfb16
elmartinezc <elmartinezc@users.noreply.github.com>
2026-04-16 07:53:38 +0000

    feat: update generated client

--- packages/mittwald/src/generated/v2/types.ts
+++ packages/mittwald/src/generated/v2/types.ts
@@ -4820,6 +4820,7 @@ export declare module MittwaldAPIV2 {
           [k: string]: MittwaldAPIV2.Components.Schemas.AppLockPurpose;
         };
         phase: MittwaldAPIV2.Components.Schemas.AppPhase;
+        projectDescription: string;
         projectId: string;
         screenshotId?: string;
         screenshotRef?: string;
