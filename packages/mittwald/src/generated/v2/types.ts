3f7dc58f97fe022ba25a153a0cbc385836bdf12b
martin-helmich <martin-helmich@users.noreply.github.com>
2026-05-06 19:34:51 +0000

    feat: update generated client

--- packages/mittwald/src/generated/v2/types.ts
+++ packages/mittwald/src/generated/v2/types.ts
@@ -12500,6 +12500,7 @@ export declare module MittwaldAPIV2 {
             withExportsOnly?: boolean;
             sortOrder?: MittwaldAPIV2.Components.Schemas.BackupBackupSortOrder;
             runningRestoresOnly?: boolean;
+            runningBackupsOnly?: boolean;
             limit?: number;
             skip?: number;
             page?: number;
@@ -17266,6 +17267,7 @@ export declare module MittwaldAPIV2 {
           export interface RequestBody {
             categoryId?: string;
             mainUserId?: string;
+            messageContent?: string;
             notificationRoles?: MittwaldAPIV2.Components.Schemas.ConversationNotificationRole[];
             relatedTo?: MittwaldAPIV2.Components.Schemas.ConversationRelatedAggregateReference;
             sharedWith?: MittwaldAPIV2.Components.Schemas.ConversationShareableAggregateReference;
