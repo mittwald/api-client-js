558451c6f6f2d0b148eaf9c5a2ccf177232564ef
Tim Auf dem Kampe <t.aufdemkampe@mittwald.de>
2026-05-08 08:28:08 +0200

    chore: export Descriptors from generated v2 module

--- packages/mittwald/src/index.ts
+++ packages/mittwald/src/index.ts
@@ -4,5 +4,6 @@ export {
   extractTotalCountHeader,
 } from "@mittwald/api-client-commons";
 export * as Commons from "@mittwald/api-client-commons";
+export * as Descriptors from "./generated/v2/descriptors.js";
 export { MittwaldAPIClient as MittwaldAPIV2Client } from "./v2/default.js";
 export type { MittwaldAPIV2 } from "./generated/v2/types.js";
